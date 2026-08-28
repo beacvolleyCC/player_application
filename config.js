e738c8e7e0341fad03e8d792a3a5bef25effa0f0
beacvolleyCC <elteroplabda@gmail.com>
2026-08-28 23:39:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.CLUB_CONTROL_CONFIG = {
-  API_URL: '' // ide kerül majd az Apps Script Web App /exec URL
+  API_URL: 'https://script.google.com/macros/s/AKfycbyyZSJC9-eHZ-TWWSDVoaAC3ISFo2KK9uQTbegP-lNrfQ2NZTXHJLLKaA4JYUPEvkHB_g/exec' // ide kerül majd az Apps Script Web App /exec URL
 };
