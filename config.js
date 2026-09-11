1ba050865048b75fba692419275d20d0d365ad2a
beacvolleyCC <elteroplabda@gmail.com>
2026-09-11 10:54:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
 window.CLUB_CONTROL_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbyyZSJC9-eHZ-TWWSDVoaAC3ISFo2KK9uQTbegP-lNrfQ2NZTXHJLLKaA4JYUPEvkHB_g/exec' // ide kerül majd az Apps Script Web App /exec URL
+  API_URL: 'https://script.google.com/macros/s/AKfycbyyZSJC9-eHZ-TWWSDVoaAC3ISFo2KK9uQTbegP-lNrfQ2NZTXHJLLKaA4JYUPEvkHB_g/exec',
+
+  DATA_BACKEND: 'supabase',
+
+  SUPABASE_URL: 'https://snqsxlyhontaepembaic.supabase.co',
+
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_k1tiZDFFUHGu4aO8DbfIiw_YvBH-qPx'
 };
