354ba40b748425cefb0b5734966d9bd2a520d7d8
beacvolleyCC <elteroplabda@gmail.com>
2026-09-13 17:05:46 +0200

    Player V2.3.5.40 grid month labels left

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='club-control-player-v2-3-5-39-grid-date-alignment';
+const CACHE='club-control-player-v2-3-5-40-grid-month-left';
 const CORE=[
   './manifest.webmanifest',
   './icons/icon-192.png',
