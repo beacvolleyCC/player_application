74c724c7765f36864971aa57c4a7d9322c881b3c
beacvolleyCC <elteroplabda@gmail.com>
2026-09-19 14:05:51 +0200

    pipa2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='club-control-player-v2-3-8-3-paid-cell-bg';
+const CACHE='club-control-player-v2-3-8-3-paid-cell-bg-full';
 const CORE=[
   './manifest.webmanifest',
   './icons/icon-192.png',
