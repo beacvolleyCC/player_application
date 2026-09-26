1a165d4090962d472630b1630840b85807644815
beacvolleyCC <elteroplabda@gmail.com>
2026-09-26 13:51:27 +0200

    red swipe patch

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='club-control-player-v2-3-10-15-monochrome-push-settings';
+const CACHE='club-control-player-v2-3-10-16-swipe-red-tail';
 const CORE=[
   './manifest.webmanifest',
   './icons/icon-192.png',
